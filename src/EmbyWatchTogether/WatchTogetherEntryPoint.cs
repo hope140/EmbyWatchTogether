e24432cbe059c03b1332c07162cb8decd22f3059
yjt <hope140y@outlook.com>
2026-08-05 20:14:39 +0800

    feat: reduce watch together sync latency

--- src/EmbyWatchTogether/WatchTogetherEntryPoint.cs
+++ src/EmbyWatchTogether/WatchTogetherEntryPoint.cs
@@ -17,6 +17,7 @@ namespace Emby.Plugins.WatchTogether
         private readonly ISessionManager _sessionManager;
         private readonly IServerApplicationHost _applicationHost;
         private readonly IJsonSerializer _jsonSerializer;
+        private SessionBridge _bridge;
         private SyncEngine _syncEngine;
 
         public WatchTogetherEntryPoint(
@@ -51,6 +52,7 @@ namespace Emby.Plugins.WatchTogether
             plugin.Bridge = bridge;
             plugin.Issuer = issuer;
 
+            _bridge = bridge;
             _syncEngine = new SyncEngine(
                 rooms,
                 provider,
@@ -58,12 +60,56 @@ namespace Emby.Plugins.WatchTogether
                 plugin.ResolveServerId,
                 pollIntervalSeconds: plugin.Configuration.PollIntervalSeconds,
                 pauseOtherOnPlaybackStop: plugin.Configuration.PauseOtherOnPlaybackStop);
+            SubscribeToSessionChanges(bridge);
             _syncEngine.Start();
         }
 
         public void Dispose()
         {
+            UnsubscribeFromSessionChanges(_bridge);
+
             _syncEngine?.Dispose();
+            _syncEngine = null;
+
+            _bridge?.Dispose();
+            _bridge = null;
         }
+
+        private void SubscribeToSessionChanges(SessionBridge bridge)
+        {
+            bridge.PlaybackStart += OnPlaybackStart;
+            bridge.PlaybackProgress += OnPlaybackProgress;
+            bridge.PlaybackStopped += OnPlaybackStopped;
+            bridge.SessionStarted += OnSessionStarted;
+            bridge.SessionEnded += OnSessionEnded;
+            bridge.CapabilitiesChanged += OnCapabilitiesChanged;
+        }
+
+        private void UnsubscribeFromSessionChanges(SessionBridge bridge)
+        {
+            if (bridge == null)
+            {
+                return;
+            }
+
+            bridge.PlaybackStart -= OnPlaybackStart;
+            bridge.PlaybackProgress -= OnPlaybackProgress;
+            bridge.PlaybackStopped -= OnPlaybackStopped;
+            bridge.SessionStarted -= OnSessionStarted;
+            bridge.SessionEnded -= OnSessionEnded;
+            bridge.CapabilitiesChanged -= OnCapabilitiesChanged;
+        }
+
+        private void OnPlaybackStart(object sender, EventArgs e) => _syncEngine?.RequestImmediatePoll();
+
+        private void OnPlaybackProgress(object sender, EventArgs e) => _syncEngine?.RequestImmediatePoll();
+
+        private void OnPlaybackStopped(object sender, EventArgs e) => _syncEngine?.RequestImmediatePoll();
+
+        private void OnSessionStarted(object sender, SessionEventArgs e) => _syncEngine?.RequestImmediatePoll();
+
+        private void OnSessionEnded(object sender, SessionEventArgs e) => _syncEngine?.RequestImmediatePoll();
+
+        private void OnCapabilitiesChanged(object sender, SessionEventArgs e) => _syncEngine?.RequestImmediatePoll();
     }
 }
